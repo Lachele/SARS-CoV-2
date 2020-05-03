123e4d85adc41b1c566046558ff72aa602e27f44
B. Lachele Foley <lachele@gmail.com>
2020-05-03 02:46:57 -0400

    set up solvation and start new ligands

--- 0_Scripts/run_tleap.bash
+++ 0_Scripts/run_tleap.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -e ../0_Scripts/Settings.bash ] ; then
+  . ../0_Scripts/Settings.bash  ## AMBERHOME is set in here
+else 
+  echo "Cannot find settings file. Exiting."; exit 1
+fi
+
 export AMBERHOME='/programs/amber-18-with-patches'
 
 source ${AMBERHOME}/amber.sh
--- 0_Scripts/use_antechamber.bash
+++ 0_Scripts/use_antechamber.bash
@@ -2,6 +2,12 @@
 
 ##  ${1} should be a prefix for the .pdb file
 
+if [ -e ../0_Scripts/Settings.bash ] ; then
+  . ../0_Scripts/Settings.bash  ## AMBERHOME is set in here
+else 
+  echo "Cannot find settings file. Exiting."; exit 1
+fi
+
 export AMBERHOME='/programs/amber-18-with-patches'
 
 source ${AMBERHOME}/amber.sh
